374b6adea54eb47217b0f4163d094f7cf474ad8f
Thanos Giannopoulos <thanosgn@gmail.com>
2017-06-20 18:27:40 +0300

    Added arguement for var

--- hyperledger/1.0/scripts/chaincodeQuery.sh
+++ hyperledger/1.0/scripts/chaincodeQuery.sh
@@ -50,9 +50,9 @@ chaincodeQuery () {
   do
      sleep 3
      echo_b "Attempting to Query PEER$PEER ...$(($(date +%s)-starttime)) secs"
-     peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","a"]}' >&log.txt
+     peer chaincode query -C $CHANNEL_NAME -n mycc -c '{"Args":["query","'$2'"]}' >&log.txt
      test $? -eq 0 && VALUE=$(cat log.txt | awk '/Query Result/ {print $NF}')
-     test "$VALUE" = "$2" && let rc=0
+     test "$VALUE" = "$3" && let rc=0
   done
   echo
   cat log.txt
@@ -68,6 +68,6 @@ chaincodeQuery () {
 
 #Query on chaincode on Peer0/Org1
 echo_b "Querying chaincode on org1/peer0..."
-chaincodeQuery 2 "$1"
+chaincodeQuery 2 "$1" "$2"
 
 exit 0
