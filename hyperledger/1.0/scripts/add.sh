088015d7b946ff53e9f4a7316ebaba126980386d
Thanos Giannopoulos <thanosgn@gmail.com>
2017-06-21 13:55:06 +0300

    added script for add function. not tested yet

---
+++ hyperledger/1.0/scripts/add.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+echo
+echo " ============================================== "
+echo " ==========initialize businesschannel========== "
+echo " ============================================== "
+echo
+
+source scripts/header.sh
+
+CHANNEL_NAME="$1"
+: ${CHANNEL_NAME:="businesschannel"}
+: ${TIMEOUT:="60"}
+COUNTER=1
+MAX_RETRY=5
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/cacerts/ca.example.com-cert.pem
+
+echo_b "Channel name : "$CHANNEL_NAME
+
+
+setGlobals () {
+
+	if [ $1 -eq 0 -o $1 -eq 1 ] ; then
+		CORE_PEER_LOCALMSPID="Org1MSP"
+		CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+		CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+		if [ $1 -eq 0 ]; then
+			CORE_PEER_ADDRESS=peer0.org1.example.com:7051
+		else
+			CORE_PEER_ADDRESS=peer1.org1.example.com:7051
+			CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+		fi
+	else
+		CORE_PEER_LOCALMSPID="Org2MSP"
+		CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+		CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+		if [ $1 -eq 2 ]; then
+			CORE_PEER_ADDRESS=peer0.org2.example.com:7051
+		else
+			CORE_PEER_ADDRESS=peer1.org2.example.com:7051
+		fi
+	fi
+
+	env |grep CORE
+}
+
+
+add () {
+	PEER=$1
+	setGlobals $PEER
+	VAR=$2
+	VAL=$3
+	# while 'peer chaincode' command can get the orderer endpoint from the peer (if join was successful),
+	# lets supply it directly as we know it using the "-o" option
+	peer chaincode invoke -C $CHANNEL_NAME -n mycc -c '{"Args":["add","c","300"]}' >&log.txt
+	res=$?
+	cat log.txt
+	echo
+}
+
+
+# Instantiate chaincode on Peer0/Org2
+# Instantiate can only be executed once on any node
+echo_b "Instantiating chaincode on peer0/org2..."
+add 2 c 300
+
+
+echo
+echo_g "===================== All GOOD, initialization completed ===================== "
+echo
+
+echo
+echo " _____   _   _   ____  "
+echo "| ____| | \ | | |  _ \ "
+echo "|  _|   |  \| | | | | |"
+echo "| |___  | |\  | | |_| |"
+echo "|_____| |_| \_| |____/ "
+echo
+
+exit 0
