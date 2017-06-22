0106b3e9782300333f41444969662ca0c26ec3df
Thanos Giannopoulos <thanosgn@gmail.com>
2017-06-22 16:03:16 +0300

    Remove newly generated images for restaring

--- hyperledger/1.0/scripts/restart.sh
+++ hyperledger/1.0/scripts/restart.sh
@@ -1,15 +1,9 @@
 sudo docker kill $(sudo docker ps -q) &&
 echo "Killed" &&
 sudo docker rm $(sudo docker ps -aq) &&
+sudo docker rmi -f `sudo docker images|grep mycc-1.0|awk '{print $3}'`
 echo "Removed" &&
 sudo docker-compose -f docker-compose-2orgs-4peers.yaml up -d &&
 echo "Containers up" &&
-sudo docker cp e2e_cli/examples/chaincode/go/chaincode_example02/chaincode_example02.go fabric-cli:/go/src/github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 &&
-sudo docker cp e2e_cli/examples/chaincode/go/chaincode_example02/chaincode_example02.go peer1.org2.example.com:/go/src/github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 &&
-sudo docker cp e2e_cli/examples/chaincode/go/chaincode_example02/chaincode_example02.go peer0.org1.example.com:/go/src/github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 &&
-sudo docker cp e2e_cli/examples/chaincode/go/chaincode_example02/chaincode_example02.go peer1.org1.example.com:/go/src/github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 &&
-sudo docker cp e2e_cli/examples/chaincode/go/chaincode_example02/chaincode_example02.go peer0.org2.example.com:/go/src/github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 &&
-sudo docker cp e2e_cli/examples/chaincode/go/chaincode_example02/chaincode_example02.go orderer.example.com:/go/src/github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02  &&
-echo "Copied chaincode" &&
 echo "Starting cli.." &&
 sudo docker exec -it fabric-cli bash
