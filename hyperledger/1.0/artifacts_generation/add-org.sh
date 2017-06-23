b7737637deb50169e2bc5d89ed704ea01b298bbe
Dimitris Mouris <jimmouris@hotmail.com>
2017-06-23 18:49:54 +0300

    Add new organization

--- hyperledger/1.0/artifacts_generation/add-org.sh
+++ hyperledger/1.0/artifacts_generation/add-org.sh
@@ -1,28 +1,35 @@
 #! /bin/bash
 
-echo "replace configtx.yaml and crypto-config.yaml"
-cp ./peer/example2/configtx.yaml ./peer
-cp ./peer/example2/crypto-config.yaml ./peer
+# echo "replace configtx.yaml and crypto-config.yaml"
+# cp ./peer/example2/configtx.yaml ./peer
+# cp ./peer/example2/crypto-config.yaml ./peer
 
-echo "replace auto-test script "
-cp ./peer/example2/new-channel-auto-test-5-peers.sh ./peer/scripts
+# echo "replace auto-test script "
+# cp ./peer/example2/new-channel-auto-test-5-peers.sh ./peer/scripts
 
-echo "replace configtx.yaml"
-cp ./peer/configtx.yaml /etc/hyperledger/fabric
-
-echo "Generate new certificates"
-
-cryptogen generate --config=./peer/crypto-config.yaml --output ./peer/crypto
+# echo "replace configtx.yaml"
+# cp ./peer/configtx.yaml /etc/hyperledger/fabric
 
 echo "Generate new certificates"
-configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./peer/channel-artifacts/orderer_genesis.block
+
+# cryptogen generate --config=./peer/crypto-config.yaml --output ./peer/crypto
+cryptogen generate --config=/etc/hyperledger/fabric/crypto-config.yaml --output ./crypto
+
+echo "Generate new certificates"
+# configtxgen -profile ThreeOrgsOrdererGenesis -outputBlock ./peer/channel-artifacts/orderer_genesis.block
+configtxgen -profile ThreeOrgsOrdererGenesis -outputBlock ./channel-artifacts/orderer_genesis.block
 
 echo "Create the configuration tx"
-CHANNEL_NAME=newchannel
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./peer/channel-artifacts/channel.tx -channelID ${CHANNEL_NAME}
+# CHANNEL_NAME=newchannel
+CHANNEL_NAME=businesschannel
+# configtxgen -profile ThreeOrgsChannel -outputCreateChannelTx ./peer/channel-artifacts/channel.tx -channelID ${CHANNEL_NAME}
+configtxgen -profile ThreeOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID ${CHANNEL_NAME}
 
-echo "Define the anchor peer for Org1 on the channel"
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./peer/channel-artifacts/Org1MSPanchors.tx -channelID ${CHANNEL_NAME} -asOrg Org1MSP
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./peer/channel-artifacts/Org2MSPanchors.tx -channelID ${CHANNEL_NAME} -asOrg Org2MSP
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./peer/channel-artifacts/Org3MSPanchors.tx -channelID ${CHANNEL_NAME} -asOrg Org3MSP
+echo "Define the anchor peer for Zeus on the channel"
+# configtxgen -profile ThreeOrgsChannel -outputAnchorPeersUpdate ./peer/channel-artifacts/ZeusMSPanchors.tx -channelID ${CHANNEL_NAME} -asOrg ZeusMSP
+# configtxgen -profile ThreeOrgsChannel -outputAnchorPeersUpdate ./peer/channel-artifacts/TrusteesMSPanchors.tx -channelID ${CHANNEL_NAME} -asOrg TrusteesMSP
+# configtxgen -profile ThreeOrgsChannel -outputAnchorPeersUpdate ./peer/channel-artifacts/MixersMSPanchors.tx -channelID ${CHANNEL_NAME} -asOrg MixersMSP
+configtxgen -profile ThreeOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/ZeusMSPanchors.tx -channelID ${CHANNEL_NAME} -asOrg ZeusMSP
+configtxgen -profile ThreeOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/TrusteesMSPanchors.tx -channelID ${CHANNEL_NAME} -asOrg TrusteesMSP
+configtxgen -profile ThreeOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/MixersMSPanchors.tx -channelID ${CHANNEL_NAME} -asOrg MixersMSP
 
