82dfce583c7a263e54b5bf991e6c0480ab05011c
Thanos Giannopoulos <thanosgn@gmail.com>
2017-06-24 12:45:05 +0300

    Add string represenation of voters and trustees

--- hyperledger/1.0/cc_operations/election_initiation.sh
+++ hyperledger/1.0/cc_operations/election_initiation.sh
@@ -18,15 +18,25 @@ function add_trustee () {
 }
 
 function add_voters () {
+	VOTERS="["
 	while read voter; do
+		VOTERS=$VOTERS$voter","
 		add_voter $voter
 	done <$VOTERFILE
+	VOTERS="${VOTERS::-1}" # Remove last ','
+	VOTERS=$VOTERS"]"
+	set_verify 2 VOTERS $VOTERS
 }
 
 function add_trustees () {
+	TRUSTEES="["
 	while read trustee; do
+		TRUSTEES=$TRUSTEES$trustee","
 		add_trustee $trustee
 	done <$TRUSTEEFILE
+	TRUSTEES="${TRUSTEES::-1}" # Remove last ','
+	TRUSTEES=$TRUSTEES"]"
+	set_verify 2 TRUSTEES $TRUSTEES
 }
 
 # Add election information
