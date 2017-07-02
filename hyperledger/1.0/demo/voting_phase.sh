6dd4ecafe77a20db9cace75904d282083b1fa497
Dimitris Mouris <jimouris@gmail.com>
2017-07-02 20:38:57 +0300

    Minor refactoring

--- hyperledger/1.0/demo/voting_phase.sh
+++ hyperledger/1.0/demo/voting_phase.sh
@@ -12,18 +12,17 @@ EXCLUDED_VOTERS_FILE=proofs/excluded_voters.log.txt
 
 ##################### Add voting phase information #####################
 
-# /bin/bash ./cc_operations/set_verify.sh votes "$(cat $VOTES_FILE)"
 while read line; do
     /bin/bash ./cc_operations/insert_list_verify.sh "VOTES" "$line"
 done <$VOTES_FILE
 
-# /bin/bash ./cc_operations/set_verify.sh audit_requests "$(cat $AUDIT_REQUESTS_FILE)"
+/bin/bash ./cc_operations/set_verify.sh audit_requests "$(cat $AUDIT_REQUESTS_FILE)"
 
-# /bin/bash ./cc_operations/set_verify.sh audit_publications "$(cat $AUDIT_PUBLICATIONS_FILE)"
+/bin/bash ./cc_operations/set_verify.sh audit_publications "$(cat $AUDIT_PUBLICATIONS_FILE)"
 
-# add_map_fields "CAST_VOTES" "$CAST_VOTES_FILE"
+add_map_fields "CAST_VOTES" "$CAST_VOTES_FILE"
 
-# /bin/bash ./cc_operations/set_verify.sh excluded_voters "$(cat $EXCLUDED_VOTERS_FILE)"
+/bin/bash ./cc_operations/set_verify.sh excluded_voters "$(cat $EXCLUDED_VOTERS_FILE)"
 
 
 echo
